dcc10f1bc37dc3f8a4cd6f1b5dab6c0503ba4d48
Aniket Srivastava <ANIKET.SRIVASTAVA.ID@GMAIL.COM>
2025-01-09 19:40:24 +0530

    readfile

--- Node.js/index.js
+++ Node.js/index.js
@@ -1,6 +1,23 @@
-const path = require('path');
-console.log(__dirname);
-console.log(__dirname+"/index.js");
-console.log(path.join(__dirname, "../../index.js","projects"));
+// const path = require('path');
+// console.log(__dirname);
+// console.log(__dirname+"/index.js");
+// console.log(path.join(__dirname, "../../index.js","projects"));
 
 
+const fs = require('fs');
+const { log } = require('console');
+
+function main(filename) {
+    fs.readFile(filename,"utf-8",function(err,data){
+        let count = 0
+        for(i = 0 ; i<data.length;i++){
+            if(data[i] === " "){
+                count++;
+            }
+        }
+        console.log(count);
+        
+    })
+}
+
+main('a.txt')
